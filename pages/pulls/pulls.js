926fb13c3f2ce39a47865b5db7db52a82dbc396c
kezhenxu94 <kezhenxu94@163.com>
2018-11-03 20:57:30 +0800

    opt(issue,pr): optimize empty view

--- pages/pulls/pulls.js
+++ pages/pulls/pulls.js
@@ -1,14 +1,20 @@
 const github = require('../../api/github.js')
 const moment = require('../../lib/moment.js')
+const utils = require('../../utils/util.js')
+
 Page({
   data: {
     filter: 'CREATED',
     pulls: [],
     scrollTop: 0,
-    lastRefresh: moment().unix()
+    lastRefresh: moment().unix(),
+    isSignedIn: utils.isSignedIn()
   },
   
   onShow: function () {
+    this.setData({
+      isSignedIn: utils.isSignedIn()
+    })
     var lastMoment = moment(this.data.lastRefresh)
     if (this.data.scrollTop === 0 && moment().diff(lastMoment, 'minutes') >= 5) {
       wx.startPullDownRefresh({})
