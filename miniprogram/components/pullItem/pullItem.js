84bd349bb51b6fc2bc49b81f993c18efe88cb3e5
kezhenxu94 <kezhenxu94@163.com>
2019-04-14 23:29:49 +0800

    Fix bug

--- miniprogram/components/pullItem/pullItem.js
+++ miniprogram/components/pullItem/pullItem.js
@@ -9,11 +9,5 @@ Component({
   data: {},
 
   methods: {
-    toPullDetail: function() {
-      const url = this.data.pull.url
-      wx.navigateTo({
-        url: '/pages/issue-detail/issue-detail?url=' + url
-      })
-    }
   }
 })
